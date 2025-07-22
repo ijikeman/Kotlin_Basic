ce1a298bcda622aa84bb64ddde97e7f9f306bdff
ijikeman <keiji.ue@gmail.com>
2025-07-22 15:54:44 +0900

    fixed User Entity

--- 7.Simple_UserEntity_workspace/src/main/kotlin/com/example/demo/entity/User.kt
+++ 7.Simple_UserEntity_workspace/src/main/kotlin/com/example/demo/entity/User.kt
@@ -1,19 +1,14 @@
+---
 package com.example.demo.entity
 
 import jakarta.persistence.*
 
 @Entity // Hibernate用
 @Table(name = "users") // Hibernate用
-open class User() {
+data class User(
     @Id // Hibernate用
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Hibernate用
-    open var id: Long = 0
-
-    open lateinit var name: String
-    open lateinit var password: String
-
-    constructor(name: String, password: String) : this() {
-        this.name = name
-        this.password = password
-    }
-}
+    val id: Long = 0,
+    var name: String = "", // 初期値を定義しないとエラーになる
+    var password: String = "" // 初期値を定義しないとエラーになる
+)
