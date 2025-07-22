07c278e1d4c8c8000d6074d1e1a9fa5318b8ae9b
ijikeman <keiji.ue@gmail.com>
2025-07-22 14:06:40 +0900

    update entity

--- 7.Simple_UserEntity_workspace/src/main/kotlin/com/example/demo/entity/User.kt
+++ 7.Simple_UserEntity_workspace/src/main/kotlin/com/example/demo/entity/User.kt
@@ -1,18 +1,13 @@
 package com.example.demo.entity
 
-// リレーショナルデータベース（RDB）とやりとりするための「標準API仕様＝Jakarta Persistence（JPA）」を定義しているパッケージ
 import jakarta.persistence.*
 
-@Entity // エンティティクラスを示すアノテーション
-@Table(name = "users") // テーブル名を指定
+@Entity // Hibernate用
+@Table(name = "users") // Hibernate用
 data class User(
-    @Id // 主キーを示すアノテーション
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 主キーの生成戦略を指定（自動インクリメント）
-    val id: Long = 0, // 主キーのID
-
-    @Column(nullable = false, unique = true, length = 50) // カラムの設定（null不可、一意制約、最大長）
-    val name: String = "", // ユーザー名
-
-    @Column(nullable = false, length = 255)
-    val password: String = "", // パスワード
+    @Id // Hibernate用
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Hibernate用
+    val id: Long = 0,
+    val name: String,
+    var password: String
 )
