cbfc4f87145676b845bd80039826ef1c2108b6bc
ijikeman <keiji.ue@gmail.com>
2025-07-27 13:00:05 +0000

    delete modules

--- 11.MultiModuleProject_LoginForm/modules/core/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("org.jetbrains.kotlin.jvm")
-}
-
-// coreモジュールは、プロジェクト全体で共有される共通のクラスやユーティリティを配置します。
-// この段階では特定のフレームワークに依存しない、純粋なKotlinライブラリとして設定します。
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
